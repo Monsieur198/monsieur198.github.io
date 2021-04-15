d47870149d932e51d31863be63fe16169a6ea814
Sergey <mons198x@gmail.com>
2021-04-15 17:17:05 +0300

    smooth appearing

--- scripts/main.js
+++ scripts/main.js
@@ -1,76 +1,119 @@
-let mainPic = document.querySelector('.main-pic');
-let defaultPic = mainPic.src;
-let thumbnails = document.querySelectorAll('.link-image');
-let description = document.querySelector('.description');
-let defaultDescription = description.textContent;
-let userButton = document.querySelector('.user-button');
-let title = document.querySelector('.title');
-let defaultTitle = title.textContent;
-let backButton = document.querySelector('.item-6');
+'use strict';
 
-let pictureChanger = function(theme) {
-    for (let j = 0; j < thumbnails.length - 1; j++) {
-        thumbnails[j].src = 'images/' + theme + String(j+1) + 'sm.jpg';     
-    }
-};
+const mainPics = document.querySelectorAll('.main-pic'),
+    thumbnails = document.querySelectorAll('.link-image'),
+    description = document.querySelector('.description'),
+    backButton = document.querySelector('.item-6'),
+    homePage = {
+        thumbSrc: [],
+        thumbAlt: [],
+        mainPicsSrc: [],
+    };
 
-for (let i = 0; i < thumbnails.length; i++) {
-    thumbnails[i].addEventListener('mouseover', function () {
-        mainPic.src = thumbnails[i].src.slice(0, -6) + '.jpg';
-        if (backButton.classList.contains('hidden') || i===5) {
-            description.textContent = thumbnails[i].alt;
-        }
+for (let i = 0; i < thumbnails.length - 1; i++) {
+    homePage.thumbSrc.push(thumbnails[i].src);
+    homePage.thumbAlt.push(thumbnails[i].alt);
+    homePage.mainPicsSrc.push(mainPics[i].src);
+}
+
+function hideMainPics() {
+    mainPics.forEach(item => {
+        item.classList.add('hidden');
+        item.classList.remove('show', 'fade');
     });
-    thumbnails[i].addEventListener('mouseout', function () {
-        if (backButton.classList.contains('hidden')) {    
-            mainPic.src = defaultPic;
-        }
-        description.textContent = defaultDescription;
-    });
-    if (i < 5) {
-        thumbnails[i].addEventListener('click', function() {
-            if (backButton.classList.contains('hidden')) {
-                pictureChanger(thumbnails[i].dataset.theme);
-                backButton.classList.remove('hidden');
-            }
-        });
+}
+
+function showMainPic(n = 5) {
+    mainPics[n].classList.add('show', 'fade');
+    mainPics[n].classList.remove('hidden');
+}
+
+function pictureChanger(theme) {
+    for (let i = 1; i < thumbnails.length; i++) {
+        thumbnails[i-1].src = `images/${theme + String(i)}sm.jpg`;
+        mainPics[i-1].src = `images/${theme + String(i)}.jpg`;
+        thumbnails[i-1].alt = '';
     }
 }
+
+hideMainPics();
+showMainPic();
+
+thumbnails.forEach((item, i) => {
+    thumbnails[i].addEventListener('mouseover', () => {
+        hideMainPics();
+        showMainPic(i);
+        description.textContent = thumbnails[i].alt;
+    });
+    thumbnails[i].addEventListener('mouseout', () => {
+        description.textContent = '';
+    });
+    thumbnails[i].addEventListener('click', (evt) => {
+        evt.preventDefault();        
+        if (backButton.classList.contains('hidden')) {
+            backButton.classList.add('show', 'fade');
+            backButton.classList.remove('hidden');
+            pictureChanger(thumbnails[i].dataset.theme);
+        }
+    });
+});
 
 backButton.addEventListener('click', function() {
     backButton.classList.add('hidden');
-    thumbnails[0].src = 'images/mypic1sm.jpg';
-    thumbnails[1].src = 'images/mypic21sm.jpg';
-    thumbnails[2].src = 'images/cats1sm.jpg';
-    thumbnails[3].src = 'images/creatures1sm.jpg';
-    thumbnails[4].src = 'images/owners1sm.jpg';
+    backButton.classList.remove('show', 'fade');
+    for (let i = 0; i < thumbnails.length - 1; i++) {
+        thumbnails[i].src = homePage.thumbSrc[i];
+        thumbnails[i].alt = homePage.thumbAlt[i];
+        mainPics[i].src = homePage.mainPicsSrc[i];
+    }
 });
 
+// old code
 
+// let mainPic = document.querySelector('.main-pic');
+// let defaultPic = mainPic.src;
+// let thumbnails = document.querySelectorAll('.link-image');
+// let description = document.querySelector('.description');
+// let defaultDescription = description.textContent;
+// let userButton = document.querySelector('.user-button');
+// let title = document.querySelector('.title');
+// let defaultTitle = title.textContent;
+// let backButton = document.querySelector('.item-6');
 
-/* function setUserName() {                             //Спрашивание имени и кнопка
-    var myName = prompt('Пожалйста представтесь.');
-    localStorage.setItem('name', myName);
-    title.textContent = myName + ', д' + defaultTitle.slice(1);
-}
+// let pictureChanger = function(theme) {
+//     for (let j = 0; j < thumbnails.length - 1; j++) {
+//         thumbnails[j].src = 'images/' + theme + String(j+1) + 'sm.jpg';     
+//     }
+// };
 
-if (!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    var storedName = localStorage.getItem('name');
-    title.textContent = storedName + ', д' + defaultTitle.slice(1);
-}
+// for (let i = 0; i < thumbnails.length; i++) {
+//     thumbnails[i].addEventListener('mouseover', function () {
+//         mainPic.src = thumbnails[i].src.slice(0, -6) + '.jpg';
+//         if (backButton.classList.contains('hidden') || i===5) {
+//             description.textContent = thumbnails[i].alt;
+//         }
+//     });
+//     thumbnails[i].addEventListener('mouseout', function () {
+//         if (backButton.classList.contains('hidden')) {    
+//             mainPic.src = defaultPic;
+//         }
+//         description.textContent = defaultDescription;
+//     });
+//     if (i < 5) {
+//         thumbnails[i].addEventListener('click', function() {
+//             if (backButton.classList.contains('hidden')) {
+//                 pictureChanger(thumbnails[i].dataset.theme);
+//                 backButton.classList.remove('hidden');
+//             }
+//         });
+//     }
+// }
 
-userButton.onclick = function () {
-    setUserName();
-} */
-
-/*  mainPic.onclick = function() {                       //не работает.. почему?
-    if (mainPic.src == 'images/profile.jpg') {
-        mainPic.src = 'images/profile2.jpg';
-    } else if (mainPic.src == 'images/profile2.jpg') {
-        mainPic.src = 'images/profile3.jpg';
-    } else {
-        mainPic.src = 'images/profile.jpg';
-    }
-} */
+// backButton.addEventListener('click', function() {
+//     backButton.classList.add('hidden');
+//     thumbnails[0].src = 'images/mypic1sm.jpg';
+//     thumbnails[1].src = 'images/mypic21sm.jpg';
+//     thumbnails[2].src = 'images/cats1sm.jpg';
+//     thumbnails[3].src = 'images/creatures1sm.jpg';
+//     thumbnails[4].src = 'images/owners1sm.jpg';
+// });
